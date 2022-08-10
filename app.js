2b351114e0e5fe16aab01bda18d301806ea97837
潘静怡 <1003346758@qq.com>
2022-08-11 01:21:13 +0800

    project init

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+ * @Author: Pan Jingyi
+ * @Date: 2022-08-10 20:34:11
+ * @LastEditTime: 2022-08-10 22:20:28
+ */
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const log4js = require('./utils/log4j')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// app.use(() => {
+//   ctx.body = 'hello'
+// })
+
+// logger
+app.use(async (ctx, next) => {
+  await next()
+  log4js.info(`log output`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  //console.error('server error', err, ctx)
+  log4js.error(`${err.stack}`)
+});
+
+module.exports = app
